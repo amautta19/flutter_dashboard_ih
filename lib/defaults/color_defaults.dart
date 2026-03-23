f801bc49e860adfc7491c40138a7047f1aef82b1
Alvaro Molina <alvaro.molina@lindley.pe>
2026-03-23 08:52:00 -0500

    Se crea paleta de colores

---
+++ lib/defaults/color_defaults.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorDefaults{
+
+  static Color primaryBlue = const Color(0xFF0984E3);
+
+  static Color secundaryBlue = const Color(0xFF00CEC9);
+
+  static Color whitePrimary = const Color(0xFFF5F6FA);
+
+  static Color darkPrimary = const Color(0xFF1E272E);
+
+}
