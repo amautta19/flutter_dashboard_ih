ceed586af9f4c408e7f785870939ad04e8c54de5
Alvaro Molina <alvaro.molina@lindley.pe>
2026-03-25 09:41:45 -0500

    Se crea un provider para el filtro

---
+++ lib/providers/filter_day_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FilterDayProvider extends ChangeNotifier {
+  // Inicializamos con el día de hoy o una fecha por defecto
+  DateTime _selectedDate = DateTime.now();
+
+  DateTime get selectedDate => _selectedDate;
+
+  void updateDate(DateTime newDate) {
+    _selectedDate = newDate;
+    notifyListeners();
+  }
+}
