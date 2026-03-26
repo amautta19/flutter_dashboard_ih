215ee5046c118ebc2b5c21e5ebffa060e8976a3c
Alvaro Molina <alvaro.molina@lindley.pe>
2026-03-26 09:36:50 -0500

    Se actualiza a Stream

--- lib/supabase_services.dart
+++ lib/supabase_services.dart
@@ -3,36 +3,36 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseServices {
   final supabase = Supabase.instance.client;
-  // Future<List<Map<String, dynamic>>> getData() async{
-  //   final response = await supabase
-  //     .from('agua_manifold_diario_v2')
-  //     .select()
-  //     .order('fecha_operativa', ascending: false);
-  //   return response;
-  // }
   Stream<List<Map<String, dynamic>>> getData() {
     return supabase
         .from('agua_manifold_diario_v2')
         .stream(primaryKey: ['id']) // Asegúrate que 'id' sea tu PK
         .order('fecha_operativa', ascending: false);
   }
-  Future<List<Map<String, dynamic>>> getDataByDayOperative(DateTime selectedDate) async {
-    // Formateamos el inicio: 07:00:00 del día seleccionado
-    final String start = "${DateFormat('yyyy-MM-dd').format(selectedDate)} 08:00:00";
-    
-    // Calculamos el final: 07:00:00 del día siguiente
-    final DateTime nextDay = selectedDate.add(const Duration(days: 1));
-    final String end = "${DateFormat('yyyy-MM-dd').format(nextDay)} 08:00:00";
 
-    final response = await supabase
+  Stream<List<Map<String, dynamic>>> getDataByDayOperative(DateTime selectedDate) {
+    // 1. Definimos los límites de tiempo igual que antes
+    final String startStr = "${DateFormat('yyyy-MM-dd').format(selectedDate)} 08:00:00";
+    final DateTime nextDay = selectedDate.add(const Duration(days: 1));
+    final String endStr = "${DateFormat('yyyy-MM-dd').format(nextDay)} 08:00:00";
+
+    // Convertimos a DateTime para comparar más fácilmente en Dart
+    final DateTime startTime = DateTime.parse(startStr);
+    final DateTime endTime = DateTime.parse(endStr);
+
+    return supabase
         .from('agua_manifold')
-        .select()
-        .filter('_time_lima', 'gte', start)
-        .filter('_time_lima', 'lt', end) // 'lt' es "menor que" para no repetir las 07:00
-        .order('_time_lima', ascending: true);
-        
-    return response;
-  }
+        .stream(primaryKey: ['id']) // Asegúrate de que 'id' sea tu PK
+        .order('_time_lima', ascending: true)
+        .map((List<Map<String, dynamic>> event) {
+          // 2. Filtramos la lista en tiempo real en el cliente
+          return event.where((item) {
+            final DateTime itemTime = DateTime.parse(item['_time_lima']);
+            return itemTime.isAtSameMomentAs(startTime) || 
+                  (itemTime.isAfter(startTime) && itemTime.isBefore(endTime));
+          }).toList();
+        });
+}
   Future<Map<String, dynamic>?> getLastUpdate() async{
     final response = await supabase
       .from('agua_manifold')
