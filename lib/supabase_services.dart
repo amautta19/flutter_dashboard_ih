222fd49a0b30d3f98a8cece40a7a2eb2431c0c8c
Alvaro Molina <alvaro.molina@lindley.pe>
2026-03-26 08:31:23 -0500

    se crea un nuevo service para obtener la hora del ultimo dato actualizado

--- lib/supabase_services.dart
+++ lib/supabase_services.dart
@@ -10,21 +10,30 @@ class SupabaseServices {
       .order('fecha_operativa', ascending: false);
     return response;
   }
-Future<List<Map<String, dynamic>>> getDataByDayOperative(DateTime selectedDate) async {
-  // Formateamos el inicio: 07:00:00 del día seleccionado
-  final String start = "${DateFormat('yyyy-MM-dd').format(selectedDate)} 08:00:00";
-  
-  // Calculamos el final: 07:00:00 del día siguiente
-  final DateTime nextDay = selectedDate.add(const Duration(days: 1));
-  final String end = "${DateFormat('yyyy-MM-dd').format(nextDay)} 08:00:00";
+  Future<List<Map<String, dynamic>>> getDataByDayOperative(DateTime selectedDate) async {
+    // Formateamos el inicio: 07:00:00 del día seleccionado
+    final String start = "${DateFormat('yyyy-MM-dd').format(selectedDate)} 08:00:00";
+    
+    // Calculamos el final: 07:00:00 del día siguiente
+    final DateTime nextDay = selectedDate.add(const Duration(days: 1));
+    final String end = "${DateFormat('yyyy-MM-dd').format(nextDay)} 08:00:00";
 
-  final response = await supabase
+    final response = await supabase
+        .from('agua_manifold')
+        .select()
+        .filter('_time_lima', 'gte', start)
+        .filter('_time_lima', 'lt', end) // 'lt' es "menor que" para no repetir las 07:00
+        .order('_time_lima', ascending: true);
+        
+    return response;
+  }
+  Future<Map<String, dynamic>?> getLastUpdate() async{
+    final response = await supabase
       .from('agua_manifold')
-      .select()
-      .filter('_time_lima', 'gte', start)
-      .filter('_time_lima', 'lt', end) // 'lt' es "menor que" para no repetir las 07:00
-      .order('_time_lima', ascending: true);
-      
-  return response;
-}
+      .select('_time_lima')
+      .order('_time_lima', ascending: false)
+      .limit(1)
+      .single();
+    return response;
+  }
 }
