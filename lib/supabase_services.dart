4b38bb3cb5a321390230f0e9d9126821ca10163f
Alvaro Molina <alvaro.molina@lindley.pe>
2026-03-26 09:28:42 -0500

    Se modifica a un Stream para quehaga la actualización en tiempo real

--- lib/supabase_services.dart
+++ lib/supabase_services.dart
@@ -3,12 +3,18 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseServices {
   final supabase = Supabase.instance.client;
-  Future<List<Map<String, dynamic>>> getData() async{
-    final response = await supabase
-      .from('agua_manifold_diario_v2')
-      .select()
-      .order('fecha_operativa', ascending: false);
-    return response;
+  // Future<List<Map<String, dynamic>>> getData() async{
+  //   final response = await supabase
+  //     .from('agua_manifold_diario_v2')
+  //     .select()
+  //     .order('fecha_operativa', ascending: false);
+  //   return response;
+  // }
+  Stream<List<Map<String, dynamic>>> getData() {
+    return supabase
+        .from('agua_manifold_diario_v2')
+        .stream(primaryKey: ['id']) // Asegúrate que 'id' sea tu PK
+        .order('fecha_operativa', ascending: false);
   }
   Future<List<Map<String, dynamic>>> getDataByDayOperative(DateTime selectedDate) async {
     // Formateamos el inicio: 07:00:00 del día seleccionado
