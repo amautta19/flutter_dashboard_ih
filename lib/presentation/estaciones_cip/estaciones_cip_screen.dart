1b68d2c1993a90175065469c273fe34487b8cd1c
Alvaro Molina <alvaro.molina@lindley.pe>
2026-03-30 11:08:07 -0500

    Se crea una nueva ventana para las estaciones CIP

---
+++ lib/presentation/estaciones_cip/estaciones_cip_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dashboard_ih/presentation/widgets/appbar_design.dart';
+
+class EstacionesCipScreen extends StatefulWidget {
+  const EstacionesCipScreen({super.key});
+
+  @override
+  State<EstacionesCipScreen> createState() => _EstacionesCipScreenState();
+}
+
+class _EstacionesCipScreenState extends State<EstacionesCipScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppbarDesign(
+        title: 'Consumo Agua Estaciones CIP - Planta Pucusana', 
+        colorBar: Colors.greenAccent, 
+        table: 'estaciones_cip_diario'
+      ),
+      body: Column(
+        children: [
+          Text('Estaciones CIP')
+        ],
+      ),
+    );
+  }
+}
