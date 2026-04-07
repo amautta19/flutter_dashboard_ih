bb2439b0a0ec9ea1fc32f0a4a683cc3f22e676bb
Alvaro Molina <alvaro.molina@lindley.pe>
2026-04-07 08:29:00 -0500

    Se agrega los datos de horas

--- lib/presentation/osmosis/osmosis_screen.dart
+++ lib/presentation/osmosis/osmosis_screen.dart
@@ -3,7 +3,13 @@ import 'package:flutter_dashboard_ih/defaults/color_defaults.dart';
 import 'package:flutter_dashboard_ih/defaults/text_global.dart';
 import 'package:flutter_dashboard_ih/presentation/osmosis/table_osmosis.dart';
 import 'package:flutter_dashboard_ih/presentation/widgets/appbar_design.dart';
+import 'package:flutter_dashboard_ih/presentation/widgets/bar_graph_diary.dart';
+import 'package:flutter_dashboard_ih/presentation/widgets/bar_graph_hours.dart';
+import 'package:flutter_dashboard_ih/presentation/widgets/filter_day.dart';
+import 'package:flutter_dashboard_ih/presentation/widgets/filter_elements.dart';
 import 'package:flutter_dashboard_ih/presentation/widgets/navbar_design.dart';
+import 'package:flutter_dashboard_ih/providers/filter_day_provider.dart';
+import 'package:flutter_dashboard_ih/providers/filter_element_provider.dart';
 import 'package:flutter_dashboard_ih/providers/filter_month_provider.dart';
 import 'package:flutter_dashboard_ih/services/supabase_services.dart';
 import 'package:provider/provider.dart';
@@ -16,9 +22,16 @@ class OsmosisScreen extends StatefulWidget {
 
 class _OsmosisScreenState extends State<OsmosisScreen> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      context.read<FilterElementProvider>().updateColumn('MF01_pc');
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     final selectedMonth = context.watch<FilterMonthProvider>().getMonth;
-    // final windowsSize = MediaQuery.of(context).size;
+    final windowSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppbarDesign(
         title: 'Consumo Agua Ósmosis - Planta Pucusana', 
@@ -58,7 +71,63 @@ class _OsmosisScreenState extends State<OsmosisScreen> {
                       GlobalText('Registro de Consumo Diario Ósmosis', fontSize: 16, fontWeight: FontWeight.bold, color: ColorDefaults.secundaryBlue,),
                       Row(
                         children: [
-                          Expanded(child: TablaOsmosis(osmosisData: filteredData)) // acá está mi tabla el archivo
+                          Expanded(child: TablaOsmosis(osmosisData: filteredData))
+                        ],
+                      ),
+                      const SizedBox(height: 30,),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              FilterPozo(columns: ['MF01_pc', 'MF02_pc', 'MF04_pc', 'MF05_pc', 'MF06_pc']),
+                              const Spacer(),
+                              FilterDayWidget(),
+                              const SizedBox(width: 100,)
+                            ],
+                          ),
+                          SizedBox(height: 5,),
+                          Row(
+                            children: [
+                              BarGraphDiary(allData: filteredData),
+                              const Spacer(),
+                              Consumer<FilterDayProvider>(
+                                builder: (context, dayProvider, child){
+                                  return Column(
+                                    children: [
+                                      StreamBuilder(
+                                        stream: SupabaseServices().getDataByDayOperative('osmosis', dayProvider.getDate), 
+                                        builder: (context, snapshot){
+                                          if (snapshot.connectionState == ConnectionState.waiting) {
+                                            return SizedBox(
+                                              height: windowSize.height * 0.42,
+                                              width: windowSize.width * 0.38,
+                                              child: Center(child: CircularProgressIndicator(color: ColorDefaults.primaryBlue,),),
+                                            );
+                                          }
+                                          if (snapshot.hasError) {
+                                            return SizedBox(
+                                             height: windowSize.height * 0.42,
+                                              width: windowSize.width * 0.38,
+                                              child: Center(child: GlobalText('Error al obtener los datos! ${snapshot.error}', fontSize: 24, color: Colors.red,),),
+                                            );
+                                          }
+                                          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                                            // return Center(child: GlobalText('Sin datos disponibles entre ese rango de fechas', fontSize: 24));
+                                            return SizedBox(
+                                              height: windowSize.height * 0.42,
+                                              width: windowSize.width * 0.38,
+                                              child: Center(child: GlobalText('Sin datos disponibles para el día seleccionado', fontSize: 24,)),
+                                            );
+                                          }
+                                          return BarGraphHours(allData: snapshot.data!);
+                                        }
+                                      )
+                                    ],
+                                  );
+                                }
+                              )
+                            ],
+                          )
                         ],
                       )
                     ],
