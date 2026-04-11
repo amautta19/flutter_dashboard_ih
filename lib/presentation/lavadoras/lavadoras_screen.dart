6378cb74284214528969b85f354f9b8a1e47d2c2
Alvaro Molina <alvaro.molina@lindley.pe>
2026-04-11 10:31:16 -0500

    se agrega al screen

--- lib/presentation/lavadoras/lavadoras_screen.dart
+++ lib/presentation/lavadoras/lavadoras_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dashboard_ih/defaults/color_defaults.dart';
+import 'package:flutter_dashboard_ih/defaults/text_global.dart';
+import 'package:flutter_dashboard_ih/presentation/lavadoras/lavadoras_bragraph.dart';
 import 'package:flutter_dashboard_ih/presentation/widgets/appbar_design.dart';
+import 'package:flutter_dashboard_ih/presentation/widgets/filter_day.dart';
 import 'package:flutter_dashboard_ih/presentation/widgets/navbar_design.dart';
+import 'package:flutter_dashboard_ih/providers/filter_day_provider.dart';
+import 'package:flutter_dashboard_ih/providers/filter_month_provider.dart';
+import 'package:flutter_dashboard_ih/services/supabase_services.dart';
+import 'package:provider/provider.dart';
 
 class LavadorasScreen extends StatefulWidget {
   const LavadorasScreen({super.key});
@@ -12,6 +20,9 @@ class LavadorasScreen extends StatefulWidget {
 class _LavadorasScreenState extends State<LavadorasScreen> {
   @override
   Widget build(BuildContext context) {
+    final selectedMonth = context.watch<FilterMonthProvider>().getMonth;
+    final dayProvider = context.watch<FilterDayProvider>().getDate;
+    final windowSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppbarDesign(
         title: 'Consumo Agua Lavadoras - Pucusana', 
@@ -19,11 +30,46 @@ class _LavadorasScreenState extends State<LavadorasScreen> {
         table: 'tiempo_efectivo'
       ),
       drawer: NavbarDisgn(),
-      body: Column(
-        children: [
-          Text('Lavadoras')
-        ],
-      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          child: Column(
+            children: [
+              FilterDayWidget(),
+              StreamBuilder<List<dynamic>>(
+                stream: SupabaseServices().getDataByDayOperative('tiempo_efectivo', dayProvider), 
+                builder: (context, snapshot){
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return SizedBox(
+                      height: windowSize.height * 0.42,
+                      width: windowSize.width * 0.38,
+                      child: Center(child: CircularProgressIndicator(color: ColorDefaults.primaryBlue,),),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return SizedBox(
+                      height: windowSize.height * 0.42,
+                      width: windowSize.width * 0.38,
+                      child: Center(child: GlobalText('Error al obtener los datos! ${snapshot.error}', fontSize: 24, color: Colors.red,),),
+                    );
+                  }
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    // return Center(child: GlobalText('Sin datos disponibles entre ese rango de fechas', fontSize: 24));
+                    return SizedBox(
+                      height: windowSize.height * 0.42,
+                      width: windowSize.width * 0.38,
+                      child: Center(child: GlobalText('Sin datos disponibles para el día seleccionado', fontSize: 24,)),
+                    );
+                  }
+                  // --- AQUÍ ESTÁ EL CAMBIO ---
+                  return GraficaEficienciaDiaria(dataCruda: snapshot.data!);
+                  
+                }
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
