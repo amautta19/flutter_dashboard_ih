33d2784108a336896aefebfaa2b7c00ae902c99a
Alvaro Molina <alvaro.molina@lindley.pe>
2026-04-15 09:50:06 -0500

    Se agrega comentarios al código

--- lib/providers/umbrales_provider.dart
+++ lib/providers/umbrales_provider.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dashboard_ih/services/supabase_services.dart';
 
 class UmbralesProvider with ChangeNotifier{
-  List<Map<String, dynamic>> _tablaUmbrales = [];
+  List<Map<String, dynamic>> _tablaUmbrales = []; // Variable donde se guardaran los datos de los umbrales
 
-  bool _isLoading = false;
+  bool _isLoading = false; // Status de carga
 
   List<Map<String, dynamic>> get tablaUmbrales => _tablaUmbrales;
   bool get isLoading => _isLoading;
 
+  // Función para obtener los umbrales desde supabase
   Future<void> actualizarUmbrales() async{
     _isLoading = true;
     notifyListeners();
     try{
-      _tablaUmbrales = await SupabaseServices().getUmbrales();
+      _tablaUmbrales = await SupabaseServices().getUmbrales(); // Se llama al servicio de supabase
       print('Tabla umbrales cargado');
     } catch (e){
       print('Error al obtener los umbrales_ $e');
